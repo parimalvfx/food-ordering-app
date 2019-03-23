188c25001878db3e2b8252d9e20c7c20ac876358
Parimal Desai <parimalvfx@gmail.com>
2019-03-23 14:49:06 +0530

    Added app logo, search box and login button to header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react';
 import './Header.css';
+import { withStyles } from '@material-ui/core/styles';
+import FastFoodIcon from '@material-ui/icons/Fastfood'
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import SearchIcon from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+
+const styles = theme => ({
+    searchUnderline: {
+        '&:after': {
+            borderBottomColor: 'white',
+        },
+    },
+});
 
 class Header extends Component {
     render() {
+        const { classes } = this.props;
         return (
             <div>
-                Header
+
+                <header className='app-header'>
+
+                    <div className='app-logo'>
+                        <FastFoodIcon id='fast-food-icon' fontSize='large' />
+                    </div>
+
+                    <div className='search-box'>
+                        <Input
+                            id='search-box-input'
+                            classes={{
+                                underline: classes.searchUnderline,
+                            }}
+                            type='text'
+                            placeholder='Search by Restaurant Name'
+                            startAdornment={
+                                <InputAdornment position='start'>
+                                    <SearchIcon id='search-box-icon' />
+                                </InputAdornment>
+                            }
+                        />
+                    </div>
+
+                    <div className='app-login'>
+                        <Button id='login-btn' size='medium' variant='contained' color='default'>
+                            <AccountCircleIcon id='login-btn-icon' />
+                            LOGIN
+                    </Button>
+                    </div>
+
+                </header>
+
             </div>
         )
     }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
