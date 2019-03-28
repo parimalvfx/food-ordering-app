e5773992c737f637012c62023a06060bf0181a65
Parimal Desai <parimalvfx@gmail.com>
2019-03-29 02:04:46 +0530

    First pass of vertical stepper

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -2,18 +2,120 @@ import React, { Component } from 'react';
 import './Checkout.css';
 import { withStyles } from '@material-ui/core/styles';
 import Header from '../../common/header/Header';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
 
-const styles = theme => ({});
+const styles = theme => ({
+    root: {
+        width: '90%',
+    },
+    button: {
+        marginTop: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+    actionsContainer: {
+        marginBottom: theme.spacing.unit * 2,
+    },
+    resetContainer: {
+        padding: theme.spacing.unit * 3,
+    },
+});
+
+function getSteps() {
+    return ['Delivery', 'Payment'];
+}
 
 class Checkout extends Component {
+
+    // constructor() {
+    //     super();
+    //     this.state = {}
+    // };
+
+    state = {
+        activeStep: 0,
+    };
+
+    handleNext = () => {
+        this.setState(state => ({
+            activeStep: state.activeStep + 1,
+        }));
+    };
+
+    handleBack = () => {
+        this.setState(state => ({
+            activeStep: state.activeStep - 1,
+        }));
+    };
+
+    handleReset = () => {
+        this.setState({
+            activeStep: 0,
+        });
+    };
+
     render() {
+        const {classes} = this.props;
+        const steps = getSteps();
+        const {activeStep} = this.state;
+
         return (
             <div>
                 <Header />
-                <h1>Checkout page</h1>
+
+                <div className={classes.root}>
+                    <Stepper activeStep={activeStep} orientation='vertical'>
+                        {steps.map((label, index) => (
+                            <Step key={label}>
+                                <StepLabel>{label}</StepLabel>
+                                <StepContent>
+                                    <h4>Tabs</h4>
+
+                                    <div className={classes.actionsContainer}>
+                                        <div>
+                                            <Button
+                                                disabled={activeStep === 0}
+                                                onClick={this.handleBack}
+                                                className={classes.button}
+                                            >
+                                                Back
+                                            </Button>
+                                            <Button
+                                                variant='contained'
+                                                color='primary'
+                                                onClick={this.handleNext}
+                                                className={classes.button}
+                                            >
+                                                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </StepContent>
+                            </Step>
+                        ))}
+                    </Stepper>
+                    {activeStep === steps.length && (
+                        <Paper square elevation={0} className={classes.resetContainer}>
+                            <Typography>View the summary &#38; place your order now</Typography>
+                            <Button onClick={this.handleReset} className={classes.button}>
+                                CHANGE
+                            </Button>
+                        </Paper>
+                    )}
+                </div>
             </div>
-        )
+        );
     }
 }
+
+Checkout.propTypes = {
+    classes: PropTypes.object,
+};
 
 export default withStyles(styles)(Checkout);
