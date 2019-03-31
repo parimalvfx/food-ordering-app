eb789ada8977de4c6b5cbeca268f6460a32b4c47
Parimal Desai <parimalvfx@gmail.com>
2019-03-31 19:43:40 +0530

    Removed unused code and reformatted code

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -33,10 +33,7 @@ import Divider from '@material-ui/core/Divider';
 import Snackbar from '@material-ui/core/Snackbar';
 
 const styles = theme => ({
-    stepperRoot: {
-        // width: '90%',
-    },
-    button: {
+    stepperButton: {
         marginTop: theme.spacing.unit,
         marginRight: theme.spacing.unit,
     },
@@ -86,13 +83,6 @@ const styles = theme => ({
     radioGroup: {
         margin: `${theme.spacing.unit}px 0`,
     },
-    summaryCard: {
-        // float: 'right',
-        // marginLeft: '10px',
-        marginRight: '15px',
-        marginTop: '25px',
-        // width: '360px',
-    },
     summaryCardDivider: {
         marginTop: '5px',
     },
@@ -101,9 +91,6 @@ const styles = theme => ({
     },
     placeOrderButton: {
         marginTop: '20px',
-    },
-    stepperGridItem: {
-        // width: '70%',
     },
 });
 
@@ -122,7 +109,7 @@ function getSteps() {
 
 function TabContainer(props) {
     return (
-        <Typography component='div' style={{padding: 8*3}}>
+        <Typography component='div' style={{ padding: 8 * 3 }}>
             {props.children}
         </Typography>
     )
@@ -173,7 +160,7 @@ class Checkout extends Component {
         // customer existing address
         let dataCustomerAddress = null;
         let xhrCustomerAddress = new XMLHttpRequest();
-        xhrCustomerAddress.addEventListener('readystatechange', function() {
+        xhrCustomerAddress.addEventListener('readystatechange', function () {
             if (this.readyState === 4) {
                 that.setState({
                     customerExistingAddresses: JSON.parse(this.responseText).addresses,
@@ -187,7 +174,7 @@ class Checkout extends Component {
         // states request
         let dataStates = null;
         let xhrStates = new XMLHttpRequest();
-        xhrStates.addEventListener('readystatechange', function() {
+        xhrStates.addEventListener('readystatechange', function () {
             if (this.readyState === 4) {
                 that.setState({
                     states: JSON.parse(this.responseText).states,
@@ -200,7 +187,7 @@ class Checkout extends Component {
         // payment modes request
         let dataPaymentModes = null;
         let xhrPaymentModes = new XMLHttpRequest();
-        xhrPaymentModes.addEventListener('readystatechange', function() {
+        xhrPaymentModes.addEventListener('readystatechange', function () {
             if (this.readyState === 4) {
                 that.setState({
                     paymentModes: JSON.parse(this.responseText).paymentMethods,
@@ -234,7 +221,7 @@ class Checkout extends Component {
     };
 
     tabChangeHandler = (event, value) => {
-        this.setState({tabValue: value});
+        this.setState({ tabValue: value });
     };
 
     existingAddressOnClickHandler = (addressId) => {
@@ -246,56 +233,56 @@ class Checkout extends Component {
     };
 
     flatBuildingNoChangeHandler = event => {
-        this.setState({flatBuildingNo: event.target.value});
+        this.setState({ flatBuildingNo: event.target.value });
     };
 
     localityChangeHandler = event => {
-        this.setState({locality: event.target.value});
+        this.setState({ locality: event.target.value });
     };
 
     cityChangeHandler = event => {
-        this.setState({city: event.target.value});
+        this.setState({ city: event.target.value });
     };
 
     stateChangeHandler = event => {
-        this.setState({newAddressState: event.target.value});
+        this.setState({ newAddressState: event.target.value });
     };
 
     pincodeChangeHandler = event => {
-        this.setState({pincode: event.target.value});
+        this.setState({ pincode: event.target.value });
     };
 
     saveAddressOnClickHandler = () => {
         let flatBuildingNoReq = false;
         if (this.state.flatBuildingNo === '') {
-            this.setState({flatBuildingNoRequired: 'display-block'});
+            this.setState({ flatBuildingNoRequired: 'display-block' });
             flatBuildingNoReq = true;
         } else {
-            this.setState({flatBuildingNoRequired: 'display-none'});
+            this.setState({ flatBuildingNoRequired: 'display-none' });
         }
 
         let localityReq = false;
         if (this.state.locality === '') {
-            this.setState({localityRequired: 'display-block'});
+            this.setState({ localityRequired: 'display-block' });
             localityReq = true;
-        }   else {
-            this.setState({localityRequired: 'display-none'});
+        } else {
+            this.setState({ localityRequired: 'display-none' });
         }
 
         let cityReq = false;
         if (this.state.city === '') {
-            this.setState({cityRequired: 'display-block'});
+            this.setState({ cityRequired: 'display-block' });
             cityReq = true;
         } else {
-            this.setState({cityRequired: 'display-none'});
+            this.setState({ cityRequired: 'display-none' });
         }
 
         let stateReq = false;
         if (this.state.newAddressState === '') {
-            this.setState({stateRequired: 'display-block'});
+            this.setState({ stateRequired: 'display-block' });
             stateReq = true;
         } else {
-            this.setState({stateRequired: 'display-none'});
+            this.setState({ stateRequired: 'display-none' });
         }
 
         let pincodeReq = false;
@@ -306,7 +293,7 @@ class Checkout extends Component {
             });
             pincodeReq = true;
         } else {
-            this.setState({pincodeRequired: 'display-none'});
+            this.setState({ pincodeRequired: 'display-none' });
         }
 
         let validatePincode = new RegExp('^[1-9][0-9]{5}$');
@@ -339,11 +326,11 @@ class Checkout extends Component {
             'state_uuid': stateUUID
         }
         let xhrNewAddress = new XMLHttpRequest();
-        xhrNewAddress.addEventListener('readystatechange', function() {
+        xhrNewAddress.addEventListener('readystatechange', function () {
             if (this.readyState === 4) {
                 let dataCustomerAddress = null;
                 let xhrCustomerAddress = new XMLHttpRequest();
-                xhrCustomerAddress.addEventListener('readystatechange', function() {
+                xhrCustomerAddress.addEventListener('readystatechange', function () {
                     if (this.readyState === 4) {
                         that.setState({
                             customerExistingAddresses: JSON.parse(this.responseText).addresses,
@@ -363,17 +350,17 @@ class Checkout extends Component {
     };
 
     radioChangeHandler = event => {
-        this.setState({radioValue: event.target.value});
+        this.setState({ radioValue: event.target.value });
     };
 
     radioClickHandler = (paymentId) => {
-        this.setState({selectedPaymentMode: paymentId});
+        this.setState({ selectedPaymentMode: paymentId });
     };
 
     placeOrderOnClickHandler = () => {
         let that = this;
         let itemQuantities = this.state.customerCart.cartItems.map(
-            function(i) {
+            function (i) {
                 return {
                     'item_id': i.id,
                     'price': i.totalItemPrice,
@@ -391,7 +378,7 @@ class Checkout extends Component {
             'restaurant_id': this.state.customerCart.restaurantDetails.id
         }
         let xhrPlaceOrder = new XMLHttpRequest();
-        xhrPlaceOrder.addEventListener('readystatechange', function() {
+        xhrPlaceOrder.addEventListener('readystatechange', function () {
             if (this.readyState === 4) {
                 let responseText = JSON.parse(this.responseText);
                 if (responseText.status === 'ORDER SUCCESSFULLY PLACED') {
@@ -420,33 +407,35 @@ class Checkout extends Component {
             return;
         }
 
-        this.setState({openPlaceOrderMsg: false});
+        this.setState({ openPlaceOrderMsg: false });
     };
 
     render() {
-        const {classes} = this.props;
+        const { classes } = this.props;
         const steps = getSteps();
-        const {activeStep} = this.state;
-        const {tabValue} = this.state;
+        const { activeStep } = this.state;
+        const { tabValue } = this.state;
 
         return (
             <div>
                 <Header />
 
                 <Grid container={true} >
-                    <Grid item={true} xs={9} className={classes.stepperGridItem}>
-                        <div className={classes.stepperRoot}>
+                    <Grid item={true} xs={9}>
+                        <div>
                             <Stepper activeStep={activeStep} orientation='vertical'>
                                 {steps.map((label, index) => (
                                     <Step key={label}>
                                         <StepLabel>{label}</StepLabel>
                                         <StepContent>
                                             {index === 0 ?
+
+                                                // existing and new address tabs
                                                 <div className={classes.tabRoot}>
                                                     <AppBar position='static'>
                                                         <Tabs value={tabValue} onChange={this.tabChangeHandler}>
-                                                            <Tab label='EXISTING ADDRESS'/>
-                                                            <Tab label='NEW ADDRESS'/>
+                                                            <Tab label='EXISTING ADDRESS' />
+                                                            <Tab label='NEW ADDRESS' />
                                                         </Tabs>
                                                     </AppBar>
 
@@ -469,7 +458,7 @@ class Checkout extends Component {
                                                                             id={this.state[address.id] || 'unselect-address'}
                                                                             onClick={() => this.existingAddressOnClickHandler(address.id)}
                                                                             className={classes.existingAddressGridListTile}
-                                                                            classes={{tile: classes.existingAddressGridListTileTile}}
+                                                                            classes={{ tile: classes.existingAddressGridListTileTile }}
                                                                         >
 
                                                                             {/* existing address - flat/building no */}
@@ -614,6 +603,7 @@ class Checkout extends Component {
                                                 : ''
                                             }
 
+                                            {/* payment methods - radio group*/}
                                             {index === 1 ?
                                                 <div className={classes.radioRoot}>
                                                     <FormControl component='fieldset' className={classes.radioFormControl}>
@@ -637,16 +627,16 @@ class Checkout extends Component {
                                                         </RadioGroup>
                                                     </FormControl>
                                                 </div>
-
                                                 : ''
                                             }
 
+                                            {/* stepper buttons */}
                                             <div className={classes.actionsContainer}>
                                                 <div>
                                                     <Button
                                                         disabled={activeStep === 0}
                                                         onClick={this.stepperBackHandler}
-                                                        className={classes.button}
+                                                        className={classes.stepperButton}
                                                     >
                                                         Back
                                                     </Button>
@@ -654,7 +644,7 @@ class Checkout extends Component {
                                                         variant='contained'
                                                         color='primary'
                                                         onClick={this.stepperNextHandler}
-                                                        className={classes.button}
+                                                        className={classes.stepperButton}
                                                     >
                                                         {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
                                                     </Button>
@@ -669,7 +659,7 @@ class Checkout extends Component {
                                     <Typography variant='h6'>
                                         View the summary &#38; place your order now!
                                     </Typography>
-                                    <Button onClick={this.stepperResetHandler} className={classes.button}>
+                                    <Button onClick={this.stepperResetHandler} className={classes.stepperButton}>
                                         CHANGE
                                     </Button>
                                 </Paper>
@@ -677,7 +667,7 @@ class Checkout extends Component {
                         </div>
                     </Grid>
 
-                    {/* cart summary */}
+                    {/* cart summary card */}
                     <Grid item={true} xs>
                         <Card id='summary-card'>
                             <CardContent>
@@ -705,7 +695,7 @@ class Checkout extends Component {
 
                                 {/* summary - net amount */}
                                 <div className={classes.netAmount}>
-                                    Net Amount 
+                                    Net Amount
                                     <span className='right mr-8p'>
                                         <span className='width-5 checkout-grey-color'>
                                             <i className='fa fa-inr'></i>
